5f395279f1f04ecaf5ada2930681ba3d738a4f45
Mike G <mikeg@respage.com>
2022-09-03 07:03:09 -0400

    install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AppComponent} from "./app.component";
 import {AppRoutingModule} from "./app-routing.module";
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
+    StoreModule.forRoot({}, {}),
   ],
   bootstrap: [AppComponent],
 })
